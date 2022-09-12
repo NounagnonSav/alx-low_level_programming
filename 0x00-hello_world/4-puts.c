13d9759600cca58e93e864a1f599a0f11a033009
NounagnonSav <nounagnonsav@gmail.com>
2022-09-12 06:00:37 -0700

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts(Mystr);
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
+	puts(my_str);
 	return (0);
 }
