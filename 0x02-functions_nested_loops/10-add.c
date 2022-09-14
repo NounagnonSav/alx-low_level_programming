2671ca126906ac1d0a81b6576ec9c175c6b45b99
NounagnonSav <nounagnonsav@gmail.com>
2022-09-14 08:54:04 -0700

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * add - Entry point
+ * add - that checks for lowercase character
+ * Return program that prints the alphabet in lowercase, using putchar
+ * Return: Void
+ *  @a: The first number
+ *  @b: the second number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
