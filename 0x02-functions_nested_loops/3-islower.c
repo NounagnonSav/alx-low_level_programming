b441c1fb762c501be4e2c00a71b1a18e355e45fd
NounagnonSav <nounagnonsav@gmail.com>
2022-09-14 06:24:44 -0700

    Write a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,13 @@
  * _islower - that checks for lowercase character
  * Return program that prints the alphabet in lowercase, using putchar
  * Return: Void
+ * c : is char
+ * int c - an the char
  */
 int _islower(int c)
 {
 	/*  print_alphabet to print abcdefghijklmnopqrstuvwxyz */
-	if ( islower(c) )
+	if (islower(c))
 		return (1);
 	else
 		return (0);
