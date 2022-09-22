1d1a75b8989ea89d407b513cf92d810fb4a16778
NounagnonSav <nounagnonsav@gmail.com>
2022-09-22 11:00:23 -0700

    Write a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Entry point
+ * _strcat - that checks for lowercase character
+ * Return program that prints the alphabet in lowercase, using putchar
+ * Return: Void
+ * @src: The character to checks for lowercase character
+ * @dest: The character to checks for lowercase character
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return strcat( dest, src );	
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MYHEADER_H_
+#define MYHEADER_H_
+
+extern char *_strcat(char *dest, char *src);
+extern char *_strncat(char *dest, char *src, int n);
+extern char *_strncpy(char *dest, char *src, int n);
+extern int _strcmp(char *s1, char *s2);
+extern void reverse_array(int *a, int n);
+extern char *string_toupper(char *);
+extern char *cap_string(char *);
+
+#endif
